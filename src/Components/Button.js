d344234b6c16aec3c537a2a0a1c843bfab7e6500
Rahul Bhagat <rahul.bhagat@biz4solutions.com>
2024-04-29 12:45:27 +0530

    first commit

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Button({btnTitle}) {
   return (
     <div className='py-1.5 px-10 rounded-md  text-white' style={{ backgroundColor: '#B3B3B3' }}>
-       <button className='' type="submit" >{btnTitle}</button>
+          <button className='' type="submit" >{btnTitle}</button>
     </div>
   )
 }
