2c82216a30eb7ce500379133ade0d55549b49661
Rahul Bhagat <rahul.bhagat@biz4solutions.com>
2024-05-10 18:36:32 +0530

    added material

--- src/Components/AddThisMaterial.js
+++ src/Components/AddThisMaterial.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function AddThisMaterial() {
   return (
-    <div>
-        <p>This material is not in your materials list</p>
-        <p style={{color:"#2CAE66",fontWeight:"600",fontSize:"12px"}} className='cursor-pointer'>Add this material</p>
+    <div className='flex flex-row items-center'>
+        <p style={{color:"black",fontWeight:"600",fontSize:"12px"}}>This material is not available in your materials list</p>
+        <Link to="/materials/AddMaterial">
+        <p style={{color:"#2CAE66",fontWeight:"600",fontSize:"12px"}} className='cursor-pointer pl-2'>Add this material</p>
+        </Link>
     </div>
   )
 }
--- src/pages/Material/CreateBatch.js
+++ src/pages/Material/CreateBatch.js
@@ -6,52 +6,50 @@ import InputBox from "../../Components/InputBox";
 import DropDown from "../../Components/Dropdown";
 import Navbar from "../../Components/NavbarCreateBatch";
 import Button from "../../Components/Button";
-import "../../CSS/CreateBatch.css"
+import "../../CSS/CreateBatch.css";
 import { Link } from "react-router-dom";
 import { api_url } from "../../Data/Constants";
 import axios from "axios";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AddThisMaterial from "../../Components/AddThisMaterial";
 
 export default function CreateBatch() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [materialsTableData,setMaterialTableData] = useState([]);
-  const [formData,setFormData]= useState({
-    materialName :'',
-    storageLocation:'',
-    batchId:'',
-    makeOrder:'',
-    expiryDate:'',
-    quantity:'',
-    price:'',
+  const [materialsTableData, setMaterialTableData] = useState([]);
+  const [formData, setFormData] = useState({
+    materialName: "",
+    storageLocation: "",
+    batchId: "",
+    makeOrder: "",
+    expiryDate: "",
+    quantity: "",
+    price: "",
   });
+  const [showAddThisMaterial, setShowAddThisMaterial] = useState(false);
+
   const handleInputChange = (e) => {
-    console.log(e);
     const { name, value } = e.target;
-    console.log("Input value changed:", name,":",value);
-    setFormData(prevData => ({
+    setFormData((prevData) => ({
       ...prevData,
-      [name]: value
+      [name]: value,
     }));
   };
-  
-  const handleSubmit= (e)=>{
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("form Data",formData);
-    // postData();
     dispatch(addBatch(formData));
-    toast.success("Batch Successfully Added")
-    navigate("/materials")
-    
-  }
+    toast.success("Batch Successfully Added");
+    navigate("/materials");
+  };
+
   const getMaterialsTableData = async () => {
     const url = `${api_url}/material`;
     try {
       const response = await axios.get(url, {
         headers: { "ngrok-skip-browser-warning": "69420" },
       });
-
       if (response?.status === 200) {
         setMaterialTableData(response?.data);
       } else {
@@ -61,117 +59,103 @@ export default function CreateBatch() {
       console.error("Error fetching data:", error);
     }
   };
-  useEffect(()=>{
+
+  useEffect(() => {
     getMaterialsTableData();
-  },[])
-  useEffect(()=>{
-      console.log("on formData.materialName change");
-      materialsTableData.map((d)=>{
-          console.log("Inside material table Data",d.materialName.trim(),formData.materialName.trim());
-          if(d.materialName.trim() == formData.materialName.trim()){
-              console.log("at create BAth :",d.materialName);
-          }
-          else{
-             console.log("add Material");
-          }
-      })
-  },[formData.materialName])
-  return (
+  }, []);
+
+  useEffect(() => {
+    console.log("on formData.materialName change");
+    for (const d of materialsTableData) {
+      console.log("Inside material table Data", d.materialName.trim(), formData.materialName.trim());
+      if (d.materialName.trim() !== formData.materialName.trim()) {
+        console.log("add this");
+        setShowAddThisMaterial(true);
+      } else {
+        console.log("material present");
+        setShowAddThisMaterial(false);
+        break; // Break out of the loop
+      }
+    }
     
-    <form onSubmit={handleSubmit} >
+  }, [formData.materialName]);
+
+  return (
+    <form onSubmit={handleSubmit}>
       <div className="p-8 bg-white">
-        <Navbar
-          title="Create Batch"
-          btnTitle="Add Material"
-          backLink="/materials"
-        />
+        <Navbar title="Create Batch" btnTitle="Add Material" backLink="/materials" />
         <div className="grid gap-y-4 pt-8">
           <div className="grid gap-2">
             <div className="grid  grid-cols-1 md:grid-cols-3 grid-flow-row gap-x-8 gap-y-8">
+              <div>
               <InputBox
                 type="text"
                 title="Material Name/Code*"
                 name="materialName"
                 onChange={handleInputChange}
-                labelCss={
-                  formData.materialName.length > 0 ? 'label-up' : 'label-down'}
+                labelCss={formData.materialName.length > 0 ? "label-up" : "label-down"}
               />
-
+              {showAddThisMaterial && <AddThisMaterial />}
+              </div>
               <DropDown
                 title="Storage Location*"
                 name="storageLocation"
                 onChange={handleInputChange}
-                labelCss={
-                  formData.storageLocation.length > 0 ? 'label-up' : 'label-down'}
+                labelCss={formData.storageLocation.length > 0 ? "label-up" : "label-down"}
               />
-
               <InputBox
                 type="text"
                 title="Batch ID*"
                 name="batchId"
                 onChange={handleInputChange}
-                labelCss={
-                  formData.batchId.length > 0 ? 'label-up' : 'label-down'}
+                labelCss={formData.batchId.length > 0 ? "label-up" : "label-down"}
               />
             </div>
           </div>
-
           <div className="grid gap-2">
             <div className="grid grid-cols-1 md:grid-cols-3 grid-flow-row gap-x-8 gap-y-8">
-             
               <InputBox
                 type="text"
                 title="Purchase Order#*"
                 name="makeOrder"
                 onChange={handleInputChange}
-                labelCss={
-                  formData.makeOrder.length > 0 ? 'label-up' : 'label-down'}
+                labelCss={formData.makeOrder.length > 0 ? "label-up" : "label-down"}
               />
-
               <InputBox
                 type="date"
                 title="Expiry Date*"
                 name="expiryDate"
                 onChange={handleInputChange}
-                labelCss={
-                  formData.expiryDate.length > 0 ? 'label-up' : 'label-down'}
+                labelCss={formData.expiryDate.length > 0 ? "label-up" : "label-down"}
               />
               <InputBox
                 type="number"
                 title="Quantity(Units)*"
                 name="quantity"
                 onChange={handleInputChange}
-                labelCss={
-                  formData.quantity  ? 'label-up' : 'label-down'}
+                labelCss={formData.quantity ? "label-up" : "label-down"}
               />
             </div>
           </div>
-
-
           <div className="grid gap-2">
             <div className="grid grid-cols-1 md:grid-cols-3 grid-flow-row gap-x-8 gap-y-8">
-          <InputBox
+              <InputBox
                 type="number"
                 title="Price($)*"
                 name="price"
                 onChange={handleInputChange}
-                labelCss={
-                  formData.price  ? 'label-up' : 'label-down'}
+                labelCss={formData.price ? "label-up" : "label-down"}
               />
-</div>
-</div>
-<div className="flex flex-row justify-end">
-  <Link to="/materials">
-              <Button
-                btnTitle="Save"
-                className=" pt-0 pb-0 text-style"
-                type="submit"
-                onClickfunction={handleSubmit}
-              />
-              </Link>
             </div>
+          </div>
+          <div className="flex flex-row justify-end">
+            <Link to="/materials">
+              <Button btnTitle="Save" className="pt-0 pb-0 text-style" type="submit" onClickfunction={handleSubmit} />
+            </Link>
+          </div>
         </div>
       </div>
+      {/* {showAddThisMaterial && <AddThisMaterial />} */}
     </form>
   );
 }
